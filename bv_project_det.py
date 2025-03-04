dad889bc9e7b05dce035af5c91c0fd364b56ee79
mhaekaal <129404191+mhaekaal@users.noreply.github.com>
2025-03-04 11:03:48 +0700

    Update bv_project_det.py

--- bv_project_det.py
+++ bv_project_det.py
@@ -9,7 +9,6 @@ import pickle
 
 
 
-
 def bv_project_det():
     st.markdown(
         '''
@@ -27,225 +26,85 @@ def bv_project_det():
         ''',
         unsafe_allow_html=True
     )
-    st.markdown(
-        '''
-            <h1 style='text-align: left; font-size:20px;'>
-               Introduction
-            </h1>
-        ''',
-        unsafe_allow_html=True
-    )
-    st.markdown(
-        '''<h1 style='text-align:left;font-size:15px;font-weight:normal;text-indent:30px; line-height:2;'>
+    st.subheader("Introduction")
+    st.write("""
         This project aims to develop a machine learning model that predicts hotel rental prices per night
         based on various factors such as location, amenities, demand trends, 
-        seasonality, and customer reviews. By leveraging historical booking data and external influencing factors, 
-        the model provides accurate price estimations to help hotel managers optimize pricing strategies and improve revenue management.
-        </h1>
-        ''', unsafe_allow_html=True
-    )
-    st.markdown(
-        '''
-            <h1 style='text-align: left; font-size:20px;'>
-               Goal
-            </h1>
-        ''',
-        unsafe_allow_html=True
-    )
-    st.markdown(
-        'To build a predictive model that accurately estimates hotel rental prices per night, enabling data-driven pricing decisions that enhance hotel profitability and market competitiveness.'
-    )
+        seasonality, and customer reviews. 
+        """)
+    st.subheader("Goal")
+    st.write("""
+        To build a predictive model that accurately estimates hotel rental prices per night, enabling data-driven pricing decisions that enhance hotel profitability and market competitiveness.
+    """)
 
-    st.markdown(
-        '''
-            <h1 style='text-align: left; font-size:20px;'>
-               Try to Prediction!
-            </h1>
-        ''',
-        unsafe_allow_html=True
-    )
-
-#__________________________________Predict__________________
-    # CSS box
-
+    st.subheader("Try to Predict!")
 
     col1, col2, col3 = st.columns(3)
 
     with col1:
-    # Get Bedrooms
-        bedrooms = st.number_input('Enter Number of Bedrooms:',
-        min_value= 1,
-        max_value= 10,
-        value=1,
-        placeholder='Please Insert Number'
-        )
-        
-    # Get Bathrooms
-        bathrooms = st.number_input('Enter Number of Bathrooms:',
-        min_value= 1,
-        max_value= 10,
-        value=1,
-        placeholder='Please Insert Number'
-        )
-        
-    # Get Agency
-        agency = st.selectbox(
-        'Choose Agency:',
-        ['Konang', 'Agus Weda', 'Bukit Vista'],
-    )
-        if agency == 'Konang':
-            agency = 0
-        elif agency == 'Agus Weda':
-            agency = 1
-        elif agency == 'Bukit Vista':
-            agency = 2
-        elif agency == 'Please Select':
-            agency = None
-    # Get City
-        city = st.selectbox(
-        'Choose City:',
-        ['Bali', 'Nusa Penida', 'Yogyakarta'],
-    )
-        if city == 'Bali':
-            city = 0
-        elif city == 'Nusa Penida':
-            city = 1
-        elif city == 'Yogyakarta':
-            city = 2
-        elif city == 'Please Select City':
-            city = None
- # Get area
-        area = st.selectbox(
-        'Choose Area:',
-        ['Canggu', 'Nusa Penida', 'Bingin', 'Uluwatu', 'Ungasan', 'Yogyakarta', 'Ubud', 'Jimbaran', 'Nusa Dua', 'Pecatu', 'Badung, Ungasan', 'Badung', 'Balangan', 'Umalas'],
-    )
-        if area == 'Canggu':
-            area = 0
-        elif area == 'Nusa Penida':
-            area = 1
-        elif area == 'Bingin':
-            area = 2
-        elif area == 'Uluwatu':
-            area = 3
-        elif area == 'Ungasan':
-            area = 4
-        elif area == 'Yogyakarta':
-            area = 5
-        elif area == 'Ubud':
-            area = 6
-        elif area == 'Jimbaran':
-            area = 7
-        elif area == 'Nusa Dua':
-            area = 8
-        elif area == 'Pecatu':
-            area = 9
-        elif area == 'Badung, Ungasan':
-            area = 10
-        elif area == 'Badung':
-            area = 11
-        elif area == 'Balangan':
-            area = 12
-        elif area == 'Umalas':
-            area = 13
-        elif area == 'Please Select Area':
-            area = None
-          
-    #Get number_of_guests
-        number_of_guests = st.number_input('Enter number of guest:',
-        min_value= 1,
-        max_value= 10,
-        value= 1,
-        placeholder='Please Insert Number'
-        )
-      
+        bedrooms = st.number_input("Enter Number of Bedrooms:", min_value=1, max_value=10, value=1)
+        bathrooms = st.number_input("Enter Number of Bathrooms:", min_value=1, max_value=10, value=1)
+
+        agency = st.selectbox("Choose Agency:", ["Konang", "Agus Weda", "Bukit Vista"])
+        agency_dict = {"Konang": 0, "Agus Weda": 1, "Bukit Vista": 2}
+        agency = agency_dict[agency]
+
+        city = st.selectbox("Choose City:", ["Bali", "Nusa Penida", "Yogyakarta"])
+        city_dict = {"Bali": 0, "Nusa Penida": 1, "Yogyakarta": 2}
+        city = city_dict[city]
+
+        area = st.selectbox("Choose Area:", 
+                            ["Canggu", "Nusa Penida", "Bingin", "Uluwatu", "Ungasan", "Yogyakarta", "Ubud", "Jimbaran", "Nusa Dua", "Pecatu", 
+                             "Badung, Ungasan", "Badung", "Balangan", "Umalas"])
+        area_dict = {area: i for i, area in enumerate(
+            ["Canggu", "Nusa Penida", "Bingin", "Uluwatu", "Ungasan", "Yogyakarta", "Ubud", "Jimbaran", "Nusa Dua", "Pecatu", 
+             "Badung, Ungasan", "Badung", "Balangan", "Umalas"])}
+        area = area_dict[area]
+
+        number_of_guests = st.number_input("Enter Number of Guests:", min_value=1, max_value=10, value=1)
+
     with col2:
-    # feature amazing pool
-        amazing_pool_yes = 1
-        amazing_pool = st.toggle("Amazing Pool",
-                                 value=bool(amazing_pool_yes)
-        )
-    # feature Pool View
-        poolview_yes = 1
-        pool_view = st.toggle("Pool View",
-                                 value=bool(poolview_yes)
-        )
-    # feature surfing
-        surfing_yes = 1
-        surfing = st.toggle("Surfing",
-                                 value=bool(surfing_yes)
-        )
-    # feature Jungle View
-        jungleview_yes = 1
-        jungle_view = st.toggle("Jungle View",
-                                 value=bool(jungleview_yes)
-        )
-    # feature Island life
-        islandlife_yes = 1
-        islandlife = st.toggle("Island Life",
-                                 value=bool(islandlife_yes)
-        )
-    # feature guest house
-        guest_house_yes = 1
-        guest_house = st.toggle("Guest House",
-                                 value=bool(guest_house_yes)
-        )
+        amazing_pool = st.toggle("Amazing Pool", value=False)
+        pool_view = st.toggle("Pool View", value=False)
+        surfing = st.toggle("Surfing", value=False)
+        jungle_view = st.toggle("Jungle View", value=False)
+        islandlife = st.toggle("Island Life", value=False)
+        guest_house = st.toggle("Guest House", value=False)
 
     with col3:
-        # feature rice paddy view
-            ricepaddyview_yes = 1
-            rice_paddy_view = st.toggle("Rice Paddy View",
-                                     value=bool(ricepaddyview_yes)
-            )
-        # feature Tropical
-            tropical_yes = 1
-            tropical = st.toggle("Tropical",
-                                     value=bool(tropical_yes)
-            )
-        # feature Ocean View
-            oceanview_yes = 1
-            ocean_view = st.toggle("Ocean View",
-                                     value=bool(oceanview_yes)
-            )
-        # feature Beachfront
-            beachfront_yes = 1
-            beachfront = st.toggle("Beach Front",
-                                     value=bool(beachfront_yes)
-            )
-        # feature Golfing
-            golfing_yes = 1
-            golfing = st.toggle("Golfing",
-                                     value=bool(golfing_yes)
-            )
-        # feature Amazing View
-            amazing_view_yes = 1
-            amazing_view = st.toggle("Golfing",
-                                     value=bool(amazing_view_yes)
-            )
-          #Calling the model and scaler
-    data = [bedrooms, bathrooms, agency, city, area, number_of_guests,
-            amazing_pool, pool_view, surfing, jungle_view, islandlife, guest_house,
-            rice_paddy_view, tropical, ocean_view, beachfront, golfing, amazing_view]
-        
-    with open("scaler.pkl", "rb") as scaler:
-        scaler = pickle.load(scaler)
-    with open("modelDT-detail.pkl", "rb") as model:
-        model = pickle.load(model)
-        columns=['bedrooms', 'bathrooms', 'agency', 'city', 'area', 'number_of_guests',
-               'Amazing pool', 'Pool view', 'Surfing', 'Jungle View', 'Island life','Guest House',
-               'Rice paddy view', 'Tropical', 'Ocean view','Beachfront', 'Golfing', 'Amazing View']
-    
-        input_data_df = pd.DataFrame([data], columns=columns)
-    
-        std_data = scaler.transform(input_data_df)
-    
-        prediction = model.predict(std_data)
-      #prediction
-        if st.button('Predict'):
-            if None in data:
-                st.markdown(':red[Please select/input correct data.]')
-            else:
-                st.write(f'Predicted Hotel Rental: {prediction[0].round(2)} $')
+        rice_paddy_view = st.toggle("Rice Paddy View", value=False)
+        tropical = st.toggle("Tropical", value=False)
+        ocean_view = st.toggle("Ocean View", value=False)
+        beachfront = st.toggle("Beach Front", value=False)
+        golfing = st.toggle("Golfing", value=False)
+        amazing_view = st.toggle("Amazing View", value=False)
 
+    if st.button("Predict"):
+        data = [bedrooms, bathrooms, agency, city, area, number_of_guests,
+                amazing_pool, pool_view, surfing, jungle_view, islandlife, guest_house,
+                rice_paddy_view, tropical, ocean_view, beachfront, golfing, amazing_view]
+
+        # Load scaler and model
+        try:
+            with open("scaler.pkl", "rb") as f:
+                scaler = pickle.load(f)
+            with open("modelDT-detail.pkl", "rb") as f:
+                model = pickle.load(f)
+
+            columns = ['bedrooms', 'bathrooms', 'agency', 'city', 'area', 'number_of_guests',
+                       'Amazing pool', 'Pool view', 'Surfing', 'Jungle View', 'Island life', 'Guest House',
+                       'Rice paddy view', 'Tropical', 'Ocean view', 'Beachfront', 'Golfing', 'Amazing View']
+
+            input_data_df = pd.DataFrame([data], columns=columns)
+            std_data = scaler.transform(input_data_df)
+            prediction = model.predict(std_data)
+
+            st.success(f"Predicted Hotel Rental: ${prediction[0]:.2f}")
+
+        except FileNotFoundError:
+            st.error("Model or scaler file not found. Please upload `model.pkl` and `scaler.pkl`.")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
 
 
 
